60f2894669d1a4bf0756cfd030d10d0cdcf6fb9b
Techevan <techevanwang@outlook.com>
2018-09-23 16:54:21 +0800

    scheduleDetail Page added

--- pages/transportation/scheduleDetail/scheduleDetail.js
+++ pages/transportation/scheduleDetail/scheduleDetail.js
@@ -1,18 +1,35 @@
 // pages/transportation/scheduleDetail/scheduleDetail.js
+/**
+ * 初始变量：
+ */
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    markers: [{
+      iconPath: "img/location.png",
+      id: 0,
+      latitude: 31.286720,
+      longitude: 121.212030,
+      width: 30,
+      height: 30,
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.setNavigationBarTitle({
+      title: '北安跨线',
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#f0f0f0',
+    })
   },
 
   /**
